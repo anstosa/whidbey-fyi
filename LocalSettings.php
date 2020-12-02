028f8b748d93a4d799cc1a74ef7b3aa46e13166b
Ansel Santosa <anstosa@users.noreply.github.com>
2020-12-02 08:57:52 -0800

    enable cache

--- LocalSettings.php
+++ LocalSettings.php
@@ -98,7 +98,7 @@ $wgShellLocale = "C.UTF-8";
 ## Set $wgCacheDirectory to a writable directory on the web server
 ## to make your wiki go slightly faster. The directory should not
 ## be publicly accessible from the web.
-#$wgCacheDirectory = "$IP/cache";
+$wgCacheDirectory = "$IP/cache";
 
 # Site language code, should be one of the list in ./languages/data/Names.php
 $wgLanguageCode = "en";
