d91100c7766104a3fac62c0abbe1e0ccefab894e
Ansel Santosa <anstosa@users.noreply.github.com>
2020-12-03 08:41:09 -0800

    set Vector to default skin

--- LocalSettings.php
+++ LocalSettings.php
@@ -132,7 +132,7 @@ $wgGroupPermissions['*']['edit'] = false;
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, ie 'vector', 'monobook':
-$wgDefaultSkin = "chameleon";
+$wgDefaultSkin = "Vector";
 
 # Enabled skins.
 # The following skins were automatically enabled:
