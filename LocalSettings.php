b769e45a29b7545f61ccc161cabe08f4765b1a3c
Ansel Santosa <anstosa@users.noreply.github.com>
2020-12-06 16:03:15 -0800

    create thumbnails

--- LocalSettings.php
+++ LocalSettings.php
@@ -89,7 +89,7 @@ $wgMemCachedServers = [];
 ## To enable image uploads, make sure the 'images' directory
 ## is writable, then set this to true:
 $wgEnableUploads = true;
-$wgGenerateThumbnailOnParse = false;
+$wgGenerateThumbnailOnParse = true;
 $wgUseImageMagick = true;
 $wgImageMagickConvertCommand = "/usr/bin/convert";
 
